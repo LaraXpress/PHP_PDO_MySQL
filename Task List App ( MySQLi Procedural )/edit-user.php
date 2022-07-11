e698960c60a5134108d4063b3107fe3c1f04806f
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-07-11 11:56:50 +0600

    Update edit-user.php

--- Task List App ( MySQLi Procedural )/edit-user.php
+++ Task List App ( MySQLi Procedural )/edit-user.php
@@ -1,56 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CRUD Applications</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-</head>
-<body>
+<?php require_once 'includes/header.php'; ?>
     <div class="container">
-        <form class="py-4">
-            <div class="row">
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Username">
-                </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Email Address">
-                </div>
-                <div class="col">
-                    <input type="submit" class="form-control btn btn-secondary" value="Add New User">
-                </div>
-            </div>
-        </form>
-        <h2>All Users</h2>
-        <table class="table">
-            <thead class="thead-dark">
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Username</th>
-                <th scope="col">Email</th>
-                <th scope="col">Edit</th>
-                <th scope="col">Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>1</th>
-                <td>Mark</td>
-                <td>mark@gmail.com</td>
-                <td><a href="#">Edit</a></td>
-                <td><a href="#">Delete</a></td>
-              </tr>
-              <tr>
-                <th>2</th>
-                <td>John</td>
-                <td>john@gmail.com</td>
-                <td><a href="#">Edit</a></td>
-                <td><a href="#">Delete</a></td>
-              </tr>
-            </tbody>
-        </table>
+        <h2 class="pt-4">User Update</h2>
 
+        <form class="py-2">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Desired username">
+            </div>
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="Desired email address">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Enter new password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-</body>
-</html>
+<?php require_once 'includes/footer.php'; ?>
